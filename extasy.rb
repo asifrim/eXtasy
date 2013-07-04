ce61516ab19951230f4ea11abf8260a79a8075af
Alejandro Sifrim <alejandro.sifrim@esat.kuleuven.be>
2013-07-04 11:35:52 +0200

    Can now keep and resume from intermediate files, rename the output, give in multiple hpo terms at the same time (and run combine).

--- extasy.rb
+++ extasy.rb
@@ -21,25 +21,60 @@ end
 
 options = ParseArguments.parse(ARGV)
 
+unless options.resume == true
 sorted_file = Intersectbed.sort_file(options.vcf_file)
 matches_file = Intersectbed.intersect_lines(sorted_file)
 annotated_file = Tabix.scan_matches_file(matches_file)
-extasy_input_file = Endeavour.append_gene_prioritizations(annotated_file, options.geneprio_file)
-extasy_output_file = Extasy.run_extasy(extasy_input_file)
+else
+	puts "#{Time.now}: Resuming from intermediate files..."
+	annotated_file = options.vcf_file+".sorted.matches.annotated"
+	if !File.file?(annotated_file)
+		puts "#{Time.now}: Intermediate files could not be found! Run eXtasy without the -r function to generate these files."
+		exit
+	end
+end
 
-prioname = File.basename(options.geneprio_file).split(".")[0]
-newfilename = "#{options.vcf_file.gsub(/\.vcf$/,"")}_-_#{prioname}.extasy"
-File.rename("#{options.vcf_file}.sorted.matches.annotated.extasy_input.extasy_output",newfilename)
+newfilenames = []
+extasy_input_files = []
+options.geneprio_files.split(",").each do |geneprio_file|
+	puts "#{Time.now}: Prioritizing against #{geneprio_file}"
+	extasy_input_file = Endeavour.append_gene_prioritizations(annotated_file, geneprio_file)
+	extasy_input_files << extasy_input_file
+	extasy_output_file = Extasy.run_extasy(extasy_input_file)
+	prioname = File.basename(geneprio_file).split(".")[0]
+	if options.prefix == nil
+		newfilename = "#{options.vcf_file.gsub(/\.vcf$/,"")}_-_#{prioname}.extasy"
+	else
+		newfilename = "#{options.prefix}_-_#{prioname}.extasy"
+	end
+	newfilenames << newfilename
+    File.rename("#{options.vcf_file}.sorted.matches.annotated.extasy_input.extasy_output",newfilename)
+end
+
+if newfilenames.size > 1 && options.combine == true
+	puts `#{File.expand_path('../combine.rb', __FILE__)} #{newfilenames.join(" ")}`
+	newfilenames << newfilenames[0].split("_-_")[0]+".combined.extasy"
+end
+
+unless options.keep == true || options.resume == true
 File.delete(sorted_file)
 File.delete(matches_file)
 File.delete(annotated_file)
-File.delete(extasy_input_file)
+extasy_input_files.each{|x| File.delete(x)}
 puts "#{Time.now}: Intermediate files deleted..."
+end
+
+
 
 if options.zip == true
 	puts "#{Time.now}: Compressing the outputfile"
-	`gzip #{newfilename}`
+	if options.prefix == nil
+		`tar -pczf #{options.vcf_file.gsub(/\.vcf$/,"")}.extasy.tar.gz  #{newfilenames.join(" ")}`
+	else
+		`tar -pczf #{options.prefix}.extasy.tar.gz  #{newfilenames.join(" ")}`
+	end
 end
+
 puts "#{Time.now}: DONE!"
 
 
